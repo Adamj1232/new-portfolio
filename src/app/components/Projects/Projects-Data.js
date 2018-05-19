cde1f3cb204b5e79082dda792555e4dfa748a04a
Adam Carpenter <adamcarpenter86@gmail.com>
2018-05-19 13:41:25 -0600

    added ghost revue site to projects

--- src/app/components/Projects/Projects-Data.js
+++ src/app/components/Projects/Projects-Data.js
@@ -40,9 +40,9 @@ const ProjectsData = [
   },
   {
     title: 'Ghost Revue',
-    description: 'This website was created for a band out of Denver that I perform with. Built using React as well as plugins from ReverbNation and Spotify',
+    description: 'This website was created for a band out of Denver that I perform with, with the help of the guitar player and fellow Turing Grad. This site was built using React as well as plugins from ReverbNation and Spotify. Logo and landing page image was created using Photoshop and Illustrator',
     image: './Project-Images/GhostRevue.png',
-    gifLink:'http://recordit.co/LAdssCIM1f',
+    gifLink:'http://g.recordit.co/LAdssCIM1f.gif',
     gitHubRepo: 'https://github.com/Adamj1232/ghost-revue',
     siteURL: 'https://adamj1232.github.io/ghost-revue/',
     techUsed: ['Javascript ', 'React ', 'HTML5 ', 'CSS3 ', 'Spotify Player ', 'ReverbNation Show Widget '],
